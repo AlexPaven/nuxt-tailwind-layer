5d94755ba0cae36c21da18ef65014c05afdcf4e3
Alex Paven <alexandru.paven@alvaria.com>
2024-02-23 11:07:43 +0000

    testing some tailwind

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -3,7 +3,7 @@ const { myLayer } = useAppConfig()
 </script>
 
 <template>
-  <div class="rounded text-orange-500">
+  <div class="rounded border m-4 p-2 text-orange-500">
     <h1>Hello World!</h1>
     <pre>{{ myLayer }}</pre>
   </div>
